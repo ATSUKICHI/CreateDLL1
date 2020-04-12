fa8de6261dd9d41d445201ac102762f46b4cef2c
AT <atsu-opmt.0215@outlook.jp>
2020-04-12 10:46:13 +0900

    プロジェクト ファイルを追加します。

---
+++ WindowsApp/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.display = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // display
+            // 
+            this.display.Location = new System.Drawing.Point(73, 109);
+            this.display.Multiline = true;
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(908, 284);
+            this.display.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1060, 451);
+            this.Controls.Add(this.display);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox display;
+    }
+}
+
---
+++ WindowsApp/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace WindowsApp
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("MyDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        private extern static int add(int a, int b);
+
+        // ポインタはC#側ではIntPtrに
+        [DllImport("MyDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        private extern static IntPtr arr_a(int length);
+
+        [DllImport("MyDLL.dll", CallingConvention = CallingConvention.Cdecl)]
+        private extern static void arr_b(IntPtr arr, int length);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+            callAdd(display);
+            callArr_a(display);
+            callArr_b(display);
+            Console.WriteLine("test");
+            //Console.ReadKey();
+        }
+
+        // 戻り値がintの場合
+        private static void callAdd(TextBox textbox)
+        {
+            int addResult = add(1, 2);
+
+            textbox.Text = "== add ==" + "\r\n";
+            textbox.AppendText(addResult + "\r\n");
+
+            Console.WriteLine("== add ==");
+            Console.WriteLine(addResult + "\n");
+        }
+
+        // 戻り値がポインタの場合
+        private static void callArr_a(TextBox textbox)
+        {
+            const int NUM = 5;
+            int[] arr = new int[NUM];
+
+            // 戻り値のポインタをIntPtrで受け取る
+            IntPtr ptr = arr_a(NUM);
+
+            // マネージ配列へコピー
+            Marshal.Copy(ptr, arr, 0, NUM);
+
+            // これでメモリが解放される？
+            // (追記)さすがに駄目でした
+            //Marshal.FreeCoTaskMem(ptr);
+
+            textbox.AppendText("== arr_a ==" + "\r\n");
+            Console.WriteLine("== arr_a ==");
+            foreach (int n in arr)
+            {
+                textbox.AppendText(n + " ");
+                Console.Write(n + " ");
+            }
+            textbox.AppendText("\r\n");
+            Console.WriteLine("\n");
+        }
+
+        // 引数がポインタの場合
+        private static void callArr_b(TextBox textbox)
+        {
+            const int NUM = 5;
+            int[] arr = new int[NUM];
+
+            // アンマネージ配列のメモリを確保
+            IntPtr ptr = Marshal.AllocCoTaskMem(Marshal.SizeOf(typeof(int)) * NUM);
+
+            // 引数でポインタを渡す
+            arr_b(ptr, NUM);
+
+            // マネージ配列へコピー
+            Marshal.Copy(ptr, arr, 0, NUM);
+
+            // アンマネージ配列のメモリを解放
+            Marshal.FreeCoTaskMem(ptr);
+
+            textbox.AppendText("== arr_b ==" + "\r\n");
+            Console.WriteLine("== arr_b ==");
+            foreach (int n in arr)
+            {
+                textbox.AppendText(n + " ");
+                Console.Write(n + " ");
+            }
+            textbox.AppendText("\r\n");
+            Console.WriteLine("\n");
+        }
+    }
+}
